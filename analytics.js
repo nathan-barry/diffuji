9d7f608e640fe1b097959afcfab9f187452860c8
Nathan Barry <nathanbarry474@gmail.com>
2026-05-12 04:42:19 +0800

    nathan.rs gtag

--- analytics.js
+++ analytics.js
@@ -1,10 +1,10 @@
 // Google Analytics (gtag.js)
 var script = document.createElement('script');
 script.async = true;
-script.src = 'https://www.googletagmanager.com/gtag/js?id=G-9FFVFZP2EW';
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-1Y735N717D';
 document.head.appendChild(script);
 
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-9FFVFZP2EW');
+gtag('config', 'G-1Y735N717D');
